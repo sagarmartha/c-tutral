32df96c4ccf9db35348d11ac4d7c107c8d7565c0
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-16 13:22:35 +0530

    2nd commit if else in only one staement

--- Hello.c
+++ Hello.c
@@ -4,15 +4,11 @@ int main() {
   int age;
   printf("enter age :");
   scanf("%d", &age);
+
   if (age > 18) 
-  {
-    printf("adult\n");
-    printf("thry can vote");
-  }
-  else{
-    printf("not addult \n");
-  }
-  
+    printf("adult \n");
+  else
+    printf("not addult \n");  
 
   return 0;
 }
