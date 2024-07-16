1fa5c666c19bf770a99879e5f788f609c29adfec
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-16 13:42:58 +0530

    use of if,else if,else

--- Hello.c
+++ Hello.c
@@ -5,10 +5,16 @@ int main() {
   printf("enter age :");
   scanf("%d", &age);
 
-  if (age > 18) 
+  if (age >= 18) {
     printf("adult \n");
-  else
-    printf("not addult \n");  
-
+    }
+  else if (age > 13 && age <18)
+  {
+    printf("teenager \n");
+  }
+  else {
+    printf("child");
+  }
+  
   return 0;
 }
