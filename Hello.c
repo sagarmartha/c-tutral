3fb441c7b891b5f723a91d5b85b2a727a3c3befd
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-17 12:25:42 +0530

    switch statement appliyed

--- Hello.c
+++ Hello.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 #include<math.h>
+//switch statement
 int main() {
-  int age;
-  printf("enter age :");
-  scanf("%d", &age);
+  int day; // 1-mon; 2-tue; 3-wed;
+  printf("enter day(1-7) :");
+  scanf("%d", &day);
 
-  age >= 18 ? printf("adult \n") : printf("not adult \n");
-  
+switch (day){
+case 1: printf("monday\n");
+  break;
+case 2: printf("tuesday\n");
+  break;
+case 3: printf("wedneday\n");
+  break;
+case 4: printf("thurday\n");
+  break;
+case 5: printf("friday\n");
+  break;
+case 6: printf("saturday\n");
+  break;
+case 7: printf("sunday\n");
+  break;
+
+    default:
+      printf("not a valid day \n");
+}
   return 0;
 }
