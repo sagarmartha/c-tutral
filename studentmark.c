8a56e2cc907f0aaef89e98ac5a7e5df7787d9ba7
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-23 23:26:31 +0530

    solve problem using ternary operater

--- studentmark.c
+++ studentmark.c
@@ -4,23 +4,23 @@
 
 #include<stdio.h>
 #include<math.h>
-// Solve problem using if,else, else-if to save all case
+// Solve problem using ternary operator
  int main(){
    int marks ;
    printf("enter number(0-100) : ");
    scanf("%d", &marks);
 
-   if (marks >= 0 && marks <= 30 )
-   {
-     printf("FAIL \n");
-   }
-   else if (marks > 30 && marks <= 100){
-    printf("PASS \n");
-   }
-   else
-   {
-     printf("wrong marks \n");
-   }
-
+  //  if (marks >= 0 && marks <= 30 )
+  //  {
+  //    printf("FAIL \n");
+  //  }
+  //  else if (marks > 30 && marks <= 100){
+  //   printf("PASS \n");
+  //  }
+  //  else
+  //  {
+  //    printf("wrong marks \n");
+  //  }
+   marks <= 30 ? printf("FAIL \n") : printf("PASS \n");
    return 0;
   }
