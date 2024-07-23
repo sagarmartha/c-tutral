734123f6c59e6b37b2f70349b57fc8c9976e3b39
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-23 19:49:07 +0530

    solve problem using if-else

--- studentmark.c
+++ studentmark.c
@@ -1,5 +1,10 @@
+// write a program to check if 100 student passed or failed
+// marks >30 is PASS
+// marks <= 30 is FAIL
+
 #include<stdio.h>
 #include<math.h>
+// Solve problem using if-else
  int main(){
    int marks;
    printf("enter number(0-100) : ");
