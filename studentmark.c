b4adf2f5b05e92857e80799da194d9a87c4407e7
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-23 19:39:28 +0530

    solve problem using if-else

---
+++ studentmark.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+ int main(){
+   int marks;
+   printf("enter number(0-100) : ");
+   scanf("%d", &marks);
+
+   if (marks <= 30)
+   {
+     printf("FAIL \n");
+   }
+   else
+   {
+     printf("PASS \n");
+   }
+
+   return 0;
+  }
