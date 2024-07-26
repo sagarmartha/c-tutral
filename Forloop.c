1d9c5d7bb3eb4be2325faaf9e65584b66587e386
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-26 13:58:12 +0530

    loop coount can be float & characte

--- Forloop.c
+++ Forloop.c
@@ -2,11 +2,24 @@
 
 int main() {
   // interator: counter
-  for (int i = 1; i <= 100; i = i+1)
-  {
-    printf("%d \n", i);
+  // for (int i = 1; i <= 100; i = i+1)
+  // {
+  //   printf("%d \n", i);
   
+  // }
+  
+  // loop coount can be float or character
+  for (float i =1.0; i <=5.0; i++)
+  {
+    printf("%f \n", i);
   }
+  
+  for (char ch = 'A'; ch <='z'; ch++)
+  {
+    printf("%c \n", ch);
+  }
+  
+  
   return 0;
 }
 
