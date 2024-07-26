6032ba2dec7d76549cbcb479d8e1cbdbae0fc088
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-07-26 13:32:14 +0530

    increament operater out

--- increamentOPT.c
+++ increamentOPT.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 int mmain (){
   // increament operater
-  // i++ , ++i
+  // ++i (per increament)
+  // i++ (post increament)
 
   int i=1;
-  printf("%d \n", i++);
+  printf("%d \n", i++); //use, then increase the value
   printf("%d \n", i);
+
+  printf("%d\n", ++i); //increase the value, then use
+  printf("%d\n", i);
   return 0;
+
+  // output-
+  // 1
+  // 2
+
+  // 2
+  // 2
+
 
 }
