cbef48d5528af4c78fea3538d83ffdf8958153cb
sagarmartha <142737071+sagarmartha@users.noreply.github.com>
2024-08-04 15:08:20 +0530

    while loop

---
+++ whileloop.c
@@ -0,0 +1,51 @@
+// #include <stdio.h>
+
+// int main(){
+//   // int i = 1;
+//   // while (i <=5)
+//   // {
+//   //   printf("Hello world \n");
+//   //   i++;
+//   // }
+
+//   int n;
+//   printf("enter a number");
+//   scanf("%d ", &n);
+
+//   int i = 0;
+//   while (i<=n)
+//   {
+//     printf("%d \n", i);
+//     i++;
+//   }
+  
+
+//   return 0;
+// }
+#include <stdio.h>
+
+int main(){
+  // int i =0;
+  // while (i<=n)
+  // {
+  //   printf("hello world \n");
+  //   i++;
+
+  // }
+  // return 0;
+  int n;
+  printf("enter number : ");
+  scanf("%d", &n);
+
+  int i = 0;
+while (i<=n)
+{
+  printf("%d \n ",  i);
+  i++;
+
+}
+
+
+  return 0;
+
+}
